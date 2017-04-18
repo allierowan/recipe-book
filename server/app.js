61e9c9a798c7149fa4493d1f9119d107f431c432
allierowan <allie.rowan@mapbox.com>
2017-04-18 17:43:53 -0400

    getting set up

---
+++ server/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var server = express();
+
+server.set('port', process.env.PORT || 3000);
+
+server.use(express.static('client/public'));
+
+server.use(bodyParser.json());
+
+server.use(function logger(req, res, next) {
+    console.log('REQUEST sent to ', req.path);
+
+    next();
+});
+
+server.use('/api/recipes', require('./routes/recipes'));
+
+server.listen(server.get('port'), function serverStarted(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Server started at http://localhost:' + server.get('port'));
+    }
+});
